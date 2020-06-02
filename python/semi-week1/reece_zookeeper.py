c30067a231f51f54f4aca05717c7e3eb733ce74e
dreeceeastwood <53875988+dreeceeastwood@users.noreply.github.com>
2020-06-02 18:47:59 -0400

    Create reece_zookeeper.py

---
+++ python/semi-week1/reece_zookeeper.py
@@ -0,0 +1,14 @@
+print("""
+
+animals = [camel, lion, deer, goose, bat, rabbit]
+
+# write your code here
+while True:
+    x = input("Which habitat # do you need?")
+    if x!='exit':
+        print(animals[int(x)])
+    elif x == 'exit':
+        print("See you!")
+        break
+
+""")
